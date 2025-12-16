70475680d065740170b995698e14d4b18e8326ab
Shreeya Chimurkat=r <53211362+ShreeyaChimurkar@users.noreply.github.com>
2025-12-16 15:51:57 +0530

    Create datalayer.js

---
+++ js/datalayer.js
@@ -0,0 +1,15 @@
+window.digitalData = {
+  page: {
+    pageName: document.title,
+    siteSection: "Mock Telecom"
+  },
+  user: {
+    loginStatus: "guest"
+  },
+  product: {
+    productID: "PLAN_5G_999",
+    productName: "Unlimited 5G Plan",
+    price: 999
+  },
+  events: []
+};
